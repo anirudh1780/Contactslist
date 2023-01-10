d1baf58833cdfedffd6e219885fc87a2b7a16c0b
anirudh1780 <anirudh@shardings.com>
2023-01-10 16:27:37 +0530

    some done

---
+++ src/screens/ContactList.js
@@ -0,0 +1,240 @@
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+import {
+	SafeAreaView,
+	ScrollView,
+	StatusBar,
+	StyleSheet,
+	Text,
+	useColorScheme,
+	View,
+	PermissionsAndroid,
+	FlatList,
+	TextInput,
+	Dimensions,
+	Pressable,
+	Platform
+} from 'react-native';
+import Svg, {
+	Circle,
+	Ellipse,
+	G,
+	TSpan,
+	TextPath,
+	Path,
+	Polygon,
+	Polyline,
+	Line,
+	Rect,
+	Use,
+	Symbol,
+	Defs,
+	LinearGradient,
+	RadialGradient,
+	Stop,
+	ClipPath,
+	Pattern,
+	Mask
+} from 'react-native-svg';
+import Contacts from 'react-native-contacts';
+
+import Contact from '../components/Contact';
+
+export default function ContactList() {
+	const availableFirstLetters = useRef(new Set([]));
+	const contactListRef = useRef();
+
+	const [contacts, setContacts] = useState([]);
+	const [searchText, setSearchText] = useState("");
+	const [topItem, setTopItem] = useState("A");
+
+	useEffect(async () => {
+		if(Platform.OS == "android") {
+			const isPermissionGiven = await getPermission();
+			console.log(isPermissionGiven);
+			if(isPermissionGiven != "granted") return;
+		}
+		Contacts.getAll()
+				.then((newContacts) => {
+					// work with contacts
+
+					console.log(newContacts);
+					
+					let filteredContacts = newContacts.filter((contact) => contact.phoneNumbers[0]?.number.includes("+65"));
+					
+
+					filteredContacts.forEach((contact) => {
+						if (contact.displayName) {
+							availableFirstLetters.current.add(contact.displayName[0])
+						}
+					});
+
+					const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+					const alphabets = alpha.map((x) => String.fromCharCode(x));
+					const availableFirstLettersArray = Array.from(availableFirstLetters.current);
+
+					//filteredContacts.push(...alphabets)
+					alphabets.forEach((alphabet) => {
+						const isLetterAvailable = availableFirstLettersArray.includes(alphabet);
+						if (isLetterAvailable) {
+							filteredContacts.push(alphabet);
+						}
+					})
+
+					filteredContacts.sort((a, b) => {
+						const displayNameA = a.displayName || a;
+						const displayNameB = b.displayName || b;
+						if (displayNameA > displayNameB) {
+							return 1;
+						}
+						if (displayNameA <= displayNameB) {
+							return -1;
+						}
+					});
+
+					if (searchText.length >= 1) {
+						filteredContacts = filteredContacts.filter(contact => contact.displayName?.toLowerCase().includes(searchText.toLowerCase()));
+					}
+
+					setContacts(filteredContacts);
+				})
+				.catch((e) => {
+					console.log(e)
+				})
+	}, [searchText]);
+
+	async function getPermission() {
+		return await PermissionsAndroid.request(
+			PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+			{
+				'title': 'Contacts',
+				'message': 'This app would like to view your contacts.',
+				'buttonPositive': 'Please accept bare mortal'
+			}
+		)
+	}
+
+	const keyExtractor = (item, idx) => {
+		return idx;
+	};
+
+	const renderContactItem = ({ item, index }) => {
+		if (item.phoneNumbers) {
+			return <Contact displayName={item.displayName} phoneNumber={item.phoneNumbers[0]} index={index} type="contact" />;
+		} else {
+			return <Contact displayName={item} index={index} type="alphabet" />
+		}
+	};
+
+	function renderFirstLetterItem({ item }) {
+		const alphabet = String.fromCharCode(item);
+		const availableFirstLettersArray = Array.from(availableFirstLetters.current);
+		const isLetterAvailable = availableFirstLettersArray.includes(alphabet);
+		return <FirstLetterItem alphabet={alphabet} isLetterAvailable={isLetterAvailable} scrollToAlphabet={scrollToAlphabet} />
+	}
+
+	function scrollToAlphabet(alphabet) {
+		const index = contacts.findIndex(contact => contact == alphabet);
+		contactListRef.current.scrollToIndex({ animated: true, index: index });
+	}
+
+	const _onViewableItemsChanged = useCallback(({ viewableItems, changed }) => {
+		if (!viewableItems[0]) return;
+
+		const firstVisibleItem = viewableItems[0].item;
+		if (!firstVisibleItem.displayName) {
+			setTopItem(firstVisibleItem);
+		} else {
+			setTopItem(firstVisibleItem.displayName[0])
+		}
+	}, []);
+
+	const _viewabilityConfig = {
+		itemVisiblePercentThreshold: 10
+	}
+
+	return (
+		<SafeAreaView>
+			<View className="bg-[#020003] h-full py-2">
+				<View className="bg-[#171317]">
+					<View className="flex justify-center h-10 w-full items-center border-b-[#9A9B9C] py-2">
+						<Text className="text-lg font-bold text-white">Contacts</Text>
+					</View>
+					<View className="p-4 h-15">
+						<View className="rounded-2xl bg-[#2D2B30] flex flex-row h-[40px] items-center justify-start pl-2">
+							<Svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								width={16}
+								height={16}
+								fill="#716E72"
+							>
+								<Path d="M23.707 22.293l-5.969-5.969a10.016 10.016 0 10-1.414 1.414l5.969 5.969a1 1 0 001.414-1.414zM10 18a8 8 0 118-8 8.009 8.009 0 01-8 8z" />
+							</Svg>
+							<TextInput
+								className="text-[#716E72] text-[16px] w-full"
+								placeholder='Search'
+								placeholderTextColor={"#716E72"}
+								onChangeText={setSearchText}
+							/>
+						</View>
+					</View>
+					<View className="border-[#2d252d] border-t-[1px]">
+						<Text className="text-[#655365] font-semibold ml-4">{topItem}</Text>
+					</View>
+				</View>
+				<View className="flex flex-row mb-[120px] h-[80%]">
+					<FlatList
+						onViewableItemsChanged={_onViewableItemsChanged}
+						viewabilityConfig={_viewabilityConfig}
+						ref={contactListRef}
+						data={contacts}
+						onScrollToIndexFailed={info => {
+							const wait = new Promise(resolve => setTimeout(resolve, 500));
+							wait.then(() => {
+							  contactListRef.current?.scrollToIndex({ index: info.index, animated: true });
+							});
+						  }}
+						renderItem={renderContactItem}
+						keyExtractor={keyExtractor}
+						showsVerticalScrollIndicator={false}
+						className="w-[95%]"
+					/>
+					<FlatList
+						data={Array.from(Array(26)).map((e, i) => i + 65)}
+						renderItem={renderFirstLetterItem}
+						keyExtractor={keyExtractor}
+						className="h-full mx-1 mt-10"
+						contentContainerStyle={{ alignItems: "center", justifyContent: "center" }}
+					/>
+				</View>
+			</View>
+		</SafeAreaView>
+	);
+}
+
+
+function FirstLetterItem({ alphabet, isLetterAvailable, scrollToAlphabet }) {
+	const [isFocused, setIsFocused] = useState(false);
+
+	function handlePress() {
+		if (!isLetterAvailable) return;
+
+
+		setIsFocused(true);
+		scrollToAlphabet(alphabet);
+	}
+
+
+	return <Pressable onPress={handlePress}
+		onPressOut={() => setIsFocused(false)}
+		className="my-[1.5px] flex items-center justify-center">
+		{
+			isLetterAvailable ?
+				<Text className="text-xs font-semibold text-blue-500">
+					{alphabet}
+				</Text>
+				:
+				<View className="w-2 h-2 my-[1px] bg-blue-500 rounded-full"></View>
+		}
+	</Pressable>
+}
---
+++ src/screens/MapsScreen.js
@@ -0,0 +1,165 @@
+import React, { useState, useEffect, useRef } from 'react'
+import {
+	SafeAreaView,
+	ScrollView,
+	StatusBar,
+	StyleSheet,
+	Text,
+	useColorScheme,
+	View,
+	PermissionsAndroid,
+	FlatList,
+	TextInput,
+	Dimensions,
+	Pressable,
+	Button,
+	Alert
+} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
+import { apiKey, token } from "../config.js"
+
+function MapsScreen() {
+	const timeout = useRef(null);
+
+	const [searchText, setSearchText] = useState("");
+	const [searchResults, setSearchResults] = useState([]);
+	const [latitude, setLatitude] = useState("");
+	const [longitude, setLongitude] = useState("");
+	const [geoCodedLoc, setGeoCodedLoc] = useState("");
+
+
+	function getGeoCode() {
+		console.log(latitude, longitude);
+		fetch(`https://developers.onemap.sg/privateapi/commonsvc/revgeocode?location=${latitude},${longitude}&token=${token}`)
+			.then(res => res.json())
+			.then((msg) => {})
+
+				if (msg.error) {
+					console.log(msg.error);
+					Alert.alert(msg.error);
+					return;
+				}
+
+				const GeocodeInfo = msg.GeocodeInfo;
+				setGeoCodedLoc(GeocodeInfo[0].BUILDINGNAME);
+			
+	}
+
+	function handleChangeText(value) {
+		clearTimeout(timeout.current);
+		setSearchText(value);
+		timeout.current = setTimeout(() => {
+			searchFunction(value)
+		}, 2000);
+	}
+
+	function searchFunction(value) {
+		fetch(`https://developers.onemap.sg/commonapi/search?searchVal=${searchText}&returnGeom=Y&getAddrDetails=Y&pageNum=1`)
+			.then(res => res.json())
+			.then(msg => {
+				setSearchResults(msg.results)
+			})
+	}
+
+	function renderItem({ item }) {
+		return <View>
+			<Text className="text-white">{item.ADDRESS}</Text>
+			<Text className="text-white">{item.BUILDING}</Text>
+		</View>
+	}
+
+	function testTheme() {
+
+
+		fetch(`https://developers.onemap.sg/privateapi/themesvc/getAllThemesInfo?token=${token}&moreInfo=Y`)
+			.then(res => res.json())
+			.then((msg) => {
+				console.log(msg)
+			})
+	}
+
+
+	return (
+		<SafeAreaView>
+			<View className="bg-[#020003] h-full py-2 px-4">
+				<View className="bg-[#171317]">
+					<View className="flex justify-center h-10 w-full items-center border-b-[#9A9B9C] py-2">
+						<Text className="text-lg font-bold text-white">Maps</Text>
+					</View>
+				</View>
+				<View className="mb-[50px]">
+					<TextInput
+						onChangeText={setSearchText}
+						className="px-2 my-2 bg-white rounded-2xl"
+						placeholder='Seach location'
+					/>
+					<View className="my-2">
+						<Button
+							title="search function"
+							onPress={searchFunction}
+							color="#771fff"
+						/>
+					</View>
+					{/* <View className="my-2">
+						<Button
+							title="Test theme"
+							onPress={testTheme}
+							color="#64ffdb"
+						/>
+					</View> */}
+					<View className="my-2">
+						<View className="flex flex-row items-center justify-start w-full gap-x-1">
+							<TextInput
+								onChangeText={setLatitude}
+								className="w-1/3 px-2 my-2 text-black bg-white rounded-2xl"
+								placeholder='Latitude'
+								value={latitude}
+							/>
+							<TextInput
+								onChangeText={setLongitude}
+								className="w-1/3 px-2 my-2 text-black bg-white rounded-2xl"
+								placeholder='Longitude'
+								value={longitude}
+							/>
+						</View>
+						<Text className="text-white">latitude: {latitude} longitude: {longitude}</Text>
+						<Button
+							title="get geo code"
+							onPress={getGeoCode}
+							color="#841584"
+						/>
+						<Button
+							title="Sing coords"
+							onPress={() => {
+								setLatitude(1.2838)
+								setLongitude(103.8591)
+							}}
+							color="#0f6547"
+						/>
+						<Button
+							title="Current loc"
+							onPress={() => {
+								Geolocation.getCurrentPosition(info => {
+									setLatitude(parseFloat(info.coords.latitude));
+									setLongitude(parseFloat(info.coords.longitude))
+								});
+							}}
+							color="098765"
+						/>
+					</View>
+				</View>
+				<Text className="text-white border-b-2 border-white">{geoCodedLoc}</Text>
+				<Text className="text-white">Search results</Text>
+				<FlatList
+					data={searchResults}
+					keyExtractor={(item, idx) => {
+						return idx;
+					}}
+					renderItem={renderItem}
+				/>
+			</View>
+		</SafeAreaView>
+	)
+}
+
+export default MapsScreen
